b3fe24b6904d7343185818a2842c2ccc60fe3dd8
Justin Wang <justinwang61@hotmail.com>
2019-07-01 20:43:25 -0400

    [lexer] Added tests for int and char lexing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,27 @@
-import HUnit
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Main where
+
+import qualified Data.Text as T
+import           Test.HUnit
+
+import qualified Lex as L
+import qualified Token as Tok
+
+testLexInteger :: Test
+testLexInteger =
+  TestCase $ assertEqual ("Lexing: " ++ show inputStr) expected (L.lex inputStr)
+  where expected = [Tok.IntKeyword 123]
+        inputStr = (T.pack "123")
+
+testLexCharKeyword :: Test
+testLexCharKeyword =
+  TestCase $ assertEqual ("Lexing: " ++ show inputStr) expected (L.lex inputStr)
+  where expected = [Tok.CharKeyword "return"]
+        inputStr = (T.pack "return")
+
+tests :: Test
+tests = TestList [ testLexInteger
+                 , testLexCharKeyword
+                 ]
+
+main :: IO Counts
+main = runTestTT tests

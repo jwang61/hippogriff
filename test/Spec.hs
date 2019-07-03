ba586346ec553d7c084af87dc279b52d3ee3ad75
Justin Wang <justinwang61@hotmail.com>
2019-07-03 01:10:15 -0400

    Added failing tests for lexer

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,18 +9,32 @@ import qualified Token as Tok
 testLexInteger :: Test
 testLexInteger =
   TestCase $ assertEqual ("Lexing: " ++ show inputStr) expected (L.lex inputStr)
-  where expected = [Tok.IntKeyword 123]
+  where expected = [Tok.IntLiteral 123]
         inputStr = (T.pack "123")
 
 testLexCharKeyword :: Test
 testLexCharKeyword =
   TestCase $ assertEqual ("Lexing: " ++ show inputStr) expected (L.lex inputStr)
-  where expected = [Tok.CharKeyword "return"]
-        inputStr = (T.pack "return")
+  where expected = [Tok.CharKeyword "foo"]
+        inputStr = (T.pack "foo")
+
+testLexIntKeyword :: Test
+testLexIntKeyword =
+  TestCase $ assertEqual ("Lexing: " ++ show inputStr) expected (L.lex inputStr)
+  where expected = [Tok.IntKeyword 123]
+        inputStr = (T.pack "int")
+
+testLexCharAndIntKeyword :: Test
+testLexCharAndIntKeyword =
+  TestCase $ assertEqual ("Lexing: " ++ show inputStr) expected (L.lex inputStr)
+  where expected = [Tok.CharKeyword "my_var3"]
+        inputStr = (T.pack "my_var3")
 
 tests :: Test
 tests = TestList [ testLexInteger
                  , testLexCharKeyword
+                 , testLexIntKeyword
+                 , testLexCharAndIntKeyword
                  ]
 
 main :: IO Counts
